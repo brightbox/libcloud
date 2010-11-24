8f0128c3231a8cb804e218a273fd5398afe0f542
Tim Fletcher <mail@tfletcher.com>
2010-11-24 13:40:24 +0000

    Add Brightbox Cloud driver.

--- libcloud/providers.py
+++ libcloud/providers.py
@@ -61,6 +61,8 @@ DRIVERS = {
         ('libcloud.drivers.opennebula', 'OpenNebulaNodeDriver'),
     Provider.DREAMHOST:
         ('libcloud.drivers.dreamhost', 'DreamhostNodeDriver'),
+    Provider.BRIGHTBOX:
+        ('libcloud.drivers.brightbox', 'BrightboxNodeDriver'),
 }
 
 def get_driver(provider):
