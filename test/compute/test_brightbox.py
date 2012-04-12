b711bc3f4020756e0058d9c8396b34d0f82c2abc
Neil Wilson <neil@aldur.co.uk>
2012-04-12 15:07:49 +0100

    Update documents and fixtures

--- test/compute/test_brightbox.py
+++ test/compute/test_brightbox.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 import sys
 import unittest
+import base64
 from libcloud.utils.py3 import httplib
 
 try:
@@ -30,6 +31,7 @@ from test.compute import TestCaseMixin
 from test.file_fixtures import ComputeFileFixtures
 from test.secrets import BRIGHTBOX_PARAMS
 
+USER_DATA="#!/bin/sh\ntest_script.sh\n"
 
 class BrightboxTest(unittest.TestCase, TestCaseMixin):
     def setUp(self):
@@ -43,6 +45,15 @@ class BrightboxTest(unittest.TestCase, TestCaseMixin):
 
         BrightboxMockHttp.type = 'UNAUTHORIZED_CLIENT'
         self.assertRaises(InvalidCredsError, self.driver.list_nodes)
+
+    def test_invalid_api_version(self):
+        self.driver = BrightboxNodeDriver(*BRIGHTBOX_PARAMS, api_version='2.0')
+        self.assertRaises(Exception, self.driver.list_locations)
+
+    def test_other_host(self):
+        self.driver = BrightboxNodeDriver(*BRIGHTBOX_PARAMS, host='api.gbt.brightbox.com')
+        locations = self.driver.list_locations()
+        self.assertEqual(len(locations), 0)
 
     def test_list_nodes(self):
         nodes = self.driver.list_nodes()
@@ -80,7 +91,7 @@ class BrightboxTest(unittest.TestCase, TestCaseMixin):
         self.assertTrue('server_groups' in nodes[0].extra)
         self.assertTrue('started_at' in nodes[0].extra)
         self.assertTrue('created_at' in nodes[0].extra)
-        self.assertTrue('deleted_at' in nodes[0].extra)
+        self.assertFalse('deleted_at' in nodes[0].extra)
 
     def test_list_sizes(self):
         sizes = self.driver.list_sizes()
@@ -94,20 +105,21 @@ class BrightboxTest(unittest.TestCase, TestCaseMixin):
 
     def test_list_images(self):
         images = self.driver.list_images()
-        self.assertEqual(len(images), 25)
+        self.assertEqual(len(images), 3)
         self.assertEqual(images[0].id, 'img-99q79')
         self.assertEqual(images[0].name, 'CentOS 5.5 server')
+        self.assertTrue('ancestor' in images[0].extra)
+        self.assertFalse('licence_name' in images[0].extra)
 
     def test_list_images_extras(self):
         images = self.driver.list_images()
-        extra = images[24].extra
-        self.assertTrue(extra['ancestor'] is None)
+        extra = images[-1].extra
         self.assertEqual(extra['arch'], 'i686')
         self.assertFalse(extra['compatibility_mode'])
         self.assertEqual(extra['created_at'], '2012-01-22T05:36:24Z')
         self.assertTrue('description' in extra)
         self.assertEqual(extra['disk_size'], 671)
-        self.assertTrue(extra['min_ram'] is None)
+        self.assertFalse('min_ram' in extra)
         self.assertFalse(extra['official'])
         self.assertEqual(extra['owner'], 'acc-tqs4c')
         self.assertTrue(extra['public'])
@@ -116,8 +128,15 @@ class BrightboxTest(unittest.TestCase, TestCaseMixin):
         self.assertEqual(extra['status'], 'deprecated')
         self.assertEqual(extra['username'], 'ubuntu')
         self.assertEqual(extra['virtual_size'], 1025)
+        self.assertFalse('ancestor' in extra)
         self.assertFalse('licence_name' in extra)
 
+    def test_list_locations(self):
+        locations = self.driver.list_locations()
+        self.assertEqual(locations[0].id, 'zon-6mxqw')
+        self.assertEqual(locations[0].name, 'gb1-a')
+        self.assertEqual(locations[1].id, 'zon-remk1')
+        self.assertEqual(locations[1].name, 'gb1-b')
 
     def test_reboot_node_response(self):
         node = self.driver.list_nodes()[0]
@@ -131,8 +150,78 @@ class BrightboxTest(unittest.TestCase, TestCaseMixin):
         size = self.driver.list_sizes()[0]
         image = self.driver.list_images()[0]
         node = self.driver.create_node(name='Test Node', image=image, size=size)
-        self.assertEqual('srv-3a97e', node.id)
+        self.assertEqual('srv-p61uj', node.id)
         self.assertEqual('Test Node', node.name)
+        self.assertEqual('gb1-a', node.extra['zone'].name)
+
+    def test_create_node_in_location(self):
+        size = self.driver.list_sizes()[0]
+        image = self.driver.list_images()[0]
+        location = self.driver.list_locations()[1]
+        node = self.driver.create_node(name='Test Node', image=image, size=size,location = location)
+        self.assertEqual('srv-nnumd', node.id)
+        self.assertEqual('Test Node', node.name)
+        self.assertEqual('gb1-b', node.extra['zone'].name)
+
+    def test_create_node_with_user_data(self):
+        size = self.driver.list_sizes()[0]
+        image = self.driver.list_images()[0]
+        node = self.driver.create_node(name='Test Node', image=image, size=size, ex_userdata=USER_DATA)
+        self.assertEqual('gb1-a', node.extra['zone'].name)
+        self.assertEqual(USER_DATA, base64.b64decode(node.extra['user_data']))
+
+    def test_create_node_with_a_server_group(self):
+        size = self.driver.list_sizes()[0]
+        image = self.driver.list_images()[0]
+        node = self.driver.create_node(name='Test Node', image=image, size=size, ex_servergroup='grp-12345')
+        self.assertEqual('gb1-a', node.extra['zone'].name)
+        self.assertEqual(len(node.extra['server_groups']), 1)
+        self.assertEqual(node.extra['server_groups'][0]['id'], 'grp-12345')
+
+    def test_create_node_with_a_list_of_server_groups(self):
+        size = self.driver.list_sizes()[0]
+        image = self.driver.list_images()[0]
+        node = self.driver.create_node(name='Test Node', image=image, size=size, ex_servergroup=['grp-12345', 'grp-67890'])
+        self.assertEqual('gb1-a', node.extra['zone'].name)
+        self.assertEqual(len(node.extra['server_groups']), 2)
+        self.assertEqual(node.extra['server_groups'][0]['id'], 'grp-12345')
+        self.assertEqual(node.extra['server_groups'][1]['id'], 'grp-67890')
+
+    def test_list_cloud_ips(self):
+        cip_list = self.driver.ex_list_cloud_ips()
+        self.assertEqual(len(cip_list), 4)
+        self.assertEqual(cip_list[2]['status'], 'mapped')
+        cip_check = cip_list[0]
+        self.assertEqual(cip_check['id'], 'cip-tlrp3')
+        self.assertEqual(cip_check['public_ip'], '109.107.35.16')
+        self.assertEqual(cip_check['reverse_dns'], 'cip-109-107-35-16.gb1.brightbox.com')
+        self.assertEqual(cip_check['status'], 'unmapped')
+        self.assertTrue(cip_check['server'] is None)
+        self.assertTrue(cip_check['server_group'] is None)
+        self.assertTrue(cip_check['interface'] is None)
+        self.assertTrue(cip_check['load_balancer'] is None)
+
+    def test_create_cloud_ip(self):
+        cip = self.driver.ex_create_cloud_ip()
+        self.assertEqual(cip['id'], 'cip-jsjc5')
+        self.assertEqual(cip['reverse_dns'], 'cip-109-107-37-234.gb1.brightbox.com')
+
+    def test_create_cloud_ip_with_dns(self):
+        cip = self.driver.ex_create_cloud_ip('fred.co.uk')
+        self.assertEqual(cip['id'], 'cip-jsjc5')
+        self.assertEqual(cip['reverse_dns'], 'fred.co.uk')
+
+    def test_map_cloud_ip(self):
+        self.assertTrue(self.driver.ex_map_cloud_ip('cip-jsjc5', 'int-ztqbx'))
+
+    def test_unmap_cloud_ip(self):
+        self.assertTrue(self.driver.ex_unmap_cloud_ip('cip-jsjc5'))
+
+    def test_update_cloud_ip(self):
+        self.assertTrue(self.driver.ex_update_cloud_ip('cip-jsjc5', 'fred.co.uk'))
+
+    def test_destroy_cloud_ip(self):
+        self.assertTrue(self.driver.ex_destroy_cloud_ip('cip-jsjc5'))
 
 
 class BrightboxMockHttp(MockHttp):
@@ -160,10 +249,17 @@ class BrightboxMockHttp(MockHttp):
         elif method == 'POST':
             body = json.loads(body)
 
-            node = json.loads(self.fixtures.load('create_server.json'))
-
+            if 'user_data' in body and body['user_data'] != base64.b64encode(USER_DATA):
+                return self.response(httplib.BAD_REQUEST, '{"error_name":"dodgy user data", "errors": ["User data not encoded properly"]}')
+            if body.get('zone', '') == 'zon-remk1':
+                node = json.loads(self.fixtures.load('create_server_gb1_b.json'))
+            else:
+                node = json.loads(self.fixtures.load('create_server_gb1_a.json'))
             node['name'] = body['name']
-
+            if 'server_groups' in body:
+                node['server_groups'] = [{'id': x} for x in body['server_groups']]
+            if 'user_data' in body:
+                node['user_data'] = body['user_data']
             return self.response(httplib.ACCEPTED, json.dumps(node))
 
     def _1_0_servers_srv_xvpn7(self, method, url, body, headers):
@@ -176,7 +272,45 @@ class BrightboxMockHttp(MockHttp):
 
     def _1_0_zones(self, method, url, body, headers):
         if method == 'GET':
-            return self.response(httplib.OK, self.fixtures.load('list_zones.json'))
+            if headers['Host'] == 'api.gbt.brightbox.com':
+                return self.response(httplib.OK, "{}")
+            else:
+                return self.response(httplib.OK, self.fixtures.load('list_zones.json'))
+    def _2_0_zones(self, method, url, body, headers):
+        return self.response(httplib.BAD_REQUEST, '{"error_name":"unrecognised_endpoint", "errors": ["The request was for an unrecognised API endpoint"]}')
+        
+    def _1_0_cloud_ips(self, method, url, body, headers):
+        if method == 'GET':
+            return self.response(httplib.OK, self.fixtures.load('list_cloud_ips.json'))
+        elif method == 'POST':
+            body = json.loads(body)
+            node = json.loads(self.fixtures.load('create_cloud_ip.json'))
+            if 'reverse_dns' in body:
+                node['reverse_dns'] = body['reverse_dns']
+            return self.response(httplib.ACCEPTED, json.dumps(node))
+
+    def _1_0_cloud_ips_cip_jsjc5(self, method, url, body, headers):
+        if method == 'DELETE':
+            return self.response(httplib.OK, '')
+        elif method == 'PUT':
+            body = json.loads(body)
+            if body.get('reverse_dns', None) == 'fred.co.uk':
+                return self.response(httplib.OK, '')
+            else:
+                return self.response(httplib.BAD_REQUEST, '{"error_name":"bad dns", "errors": ["Bad dns"]}')
+            
+    def _1_0_cloud_ips_cip_jsjc5_map(self, method, url, body, headers):
+        if method == 'POST':
+            body = json.loads(body)
+            if 'destination' in body:
+                return self.response(httplib.ACCEPTED, '')
+            else:
+                return self.response(httplib.BAD_REQUEST, '{"error_name":"bad destination", "errors": ["Bad destination"]}')
+
+    def _1_0_cloud_ips_cip_jsjc5_unmap(self, method, url, body, headers):
+        if method == 'POST':
+            return self.response(httplib.ACCEPTED, '')
+
 
     def response(self, status, body):
         return (status, body, {'content-type': 'application/json'}, httplib.responses[status])
